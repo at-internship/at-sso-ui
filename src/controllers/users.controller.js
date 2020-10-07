8d61fb07cda7dd3b860977cf2710d459416f0500
Irving Espiritu <irving.espiritu@agilethought.com>
2020-10-07 17:02:13 -0500

    Fixing files user.service.js and user.controller.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,27 +1,3 @@
-/*const userCtrl = {};
-
-const userServiceAPI = require('../services/user.service');
-
-console.log(userCtrl);
-
-// Get All users 
-userCtrl.dashboard = async(req, res) => {
-    const responseUsers = await userServiceAPI.getAllUsers();
-    console.log("userCtrl.dashboard.getAllUsers");
-    console.log(responseUsers.data);
-    const users = responseUsers.data;
-    res.render("dashboard/all-users",{
-        users
-    });
-};
-
-
-
-console.log(userCtrl.dashboard);
-
-module.exports = userCtrl;
-*/
-
 const {getAllUsers}=require("../services/user.service");
 const userCtrl = {};
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,53 +1,9 @@
 const axios = require("axios");
 // const config = require("../config");
 //const { response } = require("../server");
-const AT_SSO_SERVICE_URI = "https://at-sso-api.herokuapp.com/api";
+const AT_SSO_SERVICE_URI = process.env.AT_SSO_SERVICE_URI;
 console.log(AT_SSO_SERVICE_URI);
 
-
-
-
-
-
-/*module.exports = {
-    getAllUsers: () =>
-        axios({
-            method: "GET",
-            url: AT_SSO_SERVICE_URI + `/v1/user`,
-            headers: {
-                "content-type": "application/json",
-            },
-        })
-        .then(function(response) {
-            console.log("getAllUsers: " + AT_SSO_SERVICE_URI + `/v1/user`);
-        })
-        .catch(function(error) {
-            console.log('Error: ' + error.message);
-        }),
-    SetuserInfo: () =>
-        axios ({
-            method: "POST",
-            url: AT_SSO_SERVICE_URI + `/v1/user`,
-            data: {
-                id: 'ObjectID',
-                name: 'String',
-                firstName: 'String',
-                lastName: 'String',
-                email: 'String',
-                password: 'String (hash)',
-                status: 'Integer 0,1',
-            },
-        })
-        .then(function(response) {
-            console.log(response);
-        })
-        .catch(function(response) {
-            console.log('Error: ' + error.message);
-        })
-}
-
-
-*/
 const USERSERVICE = {};
 USERSERVICE.getAllUsers = () => { 
     return axios ({
