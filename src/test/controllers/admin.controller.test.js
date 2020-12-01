bec82df6417e28b4ebe9b5b3f63e3748ea6867ac
Norma Canalizo <norma.illescas@agilethought.com>
2020-12-01 10:02:36 -0600

    Unit test for Controllers

---
+++ src/test/controllers/admin.controller.test.js
@@ -0,0 +1,50 @@
+/**
+ * AT SSO UI - AT Admin Controller Test.
+ * Copyright 2020 AgileThought, Inc.
+ * 
+ * Unit Test for admin-controller.
+ * 
+ * @author @at-internship
+ * @version 1.0
+ */
+
+ // Constants
+const sinon = require("sinon");
+const expect = require("chai").expect;
+
+// AT Admin Controller
+const adminController = require("../../controllers/admin.controller");
+
+// AT SSO Service API
+const ssoServiceAPI = require("../../services/at-sso-api.service");
+
+describe ('Admin Controler TEST', function(){
+
+    let getAllUsersStub;
+
+    beforeEach(function() {
+        getAllUsersStub = sinon.stub(ssoServiceAPI, "getAllUsers");
+    });
+    afterEach(function() {
+        getAllUsersStub.restore();
+    });
+
+    it("Should render admin dashboard", function(done) {
+        var res = { render: sinon.spy() };
+        var req = {};
+        var view = adminController.renderIndex(req, res).then(function() {
+            expect(res.render.calledOnce).to.be.true;
+            done();
+        });
+    });
+    it("Should render admin story list view", function(done) {
+        var res = { render: sinon.spy() };
+        var req = {};
+        var users = [];
+        getAllUsersStub.returns(Promise.resolve(users));
+        var view = adminController.renderUserList(req, res).then(function() {
+            expect(res.render.calledOnce).to.be.true;
+            done();
+        });
+    });
+});
